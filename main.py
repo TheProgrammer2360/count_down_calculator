a38a16d3c32926ed5b62f40823bd5b2f49fdc3e1
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-02 08:59:03 +0200

    changed the date input date into date objects

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import datetime
+
 # get the dates from the user
 print("**The format of the date YYYY-MM-DD")
-data1 = input("Enter the First date: ")
+date1 = input("Enter the First date: ")
 date2 = input("Enter the Second date: ")
+# change the dates to be a date object
+date1_as_datetime = datetime.datetime(year=int(date1[0:4]), month=int(date1[5:7]), day=int(date1[8:]))
+date2_as_datetime = datetime.datetime(year=int(date2[0:4]), month=int(date2[5:7]), day=int(date2[8:]))
