ae721efb8868e0d2db21733f1c255674bc8626ab
TheProgrammer <mxolisinkosi2360@gmail.com>
2023-03-02 09:06:39 +0200

    calculate the time difference in days and printed it

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ date2 = input("Enter the Second date: ")
 # change the dates to be a date object
 date1_as_datetime = datetime.datetime(year=int(date1[0:4]), month=int(date1[5:7]), day=int(date1[8:]))
 date2_as_datetime = datetime.datetime(year=int(date2[0:4]), month=int(date2[5:7]), day=int(date2[8:]))
+# getting the time difference
+time_difference = date1_as_datetime - date2_as_datetime
+print(time_difference)
